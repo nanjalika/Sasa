c37909a31d0bfb594392dbf7f6fa88941c4706b0
康南舟｜Nathanael B <141347068+nanjalika@users.noreply.github.com>
2026-05-25 21:35:11 +0300

    Add files via upload

---
+++ android/app/src/main/java/com/sasa/MainActivity.java
@@ -0,0 +1,31 @@
+package com.sasa;
+
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen;
+
+public class MainActivity extends ReactActivity {
+
+    @Override
+    protected String getMainComponentName() {
+        return "Sasa";
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+        return new DefaultReactActivityDelegate(
+            this,
+            getMainComponentName(),
+            DefaultNewArchitectureEntryPoint.getFabricEnabled()
+        );
+    }
+}
